97aafdf6f2423797522c79d4bf53656afc69549e
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2025-02-17 17:14:57 -0400

    renamed package

--- src/main/java/com/project/bookapp/BookAppApplication.java
+++ src/main/java/com/project/bookapp/BookAppApplication.java
@@ -1,4 +1,4 @@
-package com.project.bookApp;
+package com.project.bookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
