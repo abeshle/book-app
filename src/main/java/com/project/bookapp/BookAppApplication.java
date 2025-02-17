c155fa152e9364dd0e51b11c6db3f1c119c5c298
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2025-02-17 17:18:22 -0400

    reviewed checkstyle

--- src/main/java/com/project/bookapp/BookAppApplication.java
+++ src/main/java/com/project/bookapp/BookAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BookAppApplication.class, args);
+    }
 }
