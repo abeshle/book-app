ba34a086adf068ce258a198d5b0c033bf54cab7c
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2025-10-27 19:20:30 -0300

    add tests

--- src/main/java/com/project/bookapp/model/Book.java
+++ src/main/java/com/project/bookapp/model/Book.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
@@ -23,6 +24,7 @@ import org.hibernate.annotations.SQLRestriction;
 @Setter
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
 @SQLRestriction("is_deleted = false")
+@Accessors(chain = true)
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
