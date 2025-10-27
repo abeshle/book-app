63b07000267e94c9957529bfa24b3cf429bd4aa3
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2025-10-27 18:12:15 -0300

    add tests

---
+++ src/test/resources/database/add-categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (id, name)
+VALUES
+  (1, 'Adventure'),
+  (2, 'Science Fiction'),
+  (3, 'Computer Science');
---
+++ src/test/resources/database/add-three-books.sql
@@ -0,0 +1,5 @@
+INSERT INTO books (title, author, price, isbn)
+VALUES
+  ('Test Book 1', 'One', 10.1, '978-0000000001'),
+  ('Test Book 2', 'Two', 10.1, '978-0000000002'),
+  ('Test Book 3', 'Three', 10.1, '978-0000000003');
