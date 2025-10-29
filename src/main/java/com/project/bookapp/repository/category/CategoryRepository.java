002fdac7604349b51a71a714e152cd23880e23d5
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2025-10-28 21:45:21 -0300

    config

--- src/main/java/com/project/bookapp/repository/category/CategoryRepository.java
+++ src/main/java/com/project/bookapp/repository/category/CategoryRepository.java
@@ -1,7 +1,9 @@
 package com.project.bookapp.repository.category;
 
 import com.project.bookapp.model.Category;
+import lombok.experimental.Accessors;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Accessors(chain = true)
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
